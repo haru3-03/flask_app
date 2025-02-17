2e3ced0d13ce49476bf865184ec825b8cd8a93d0
haru3-03 <urq30u0gsy_3@yahoo.co.jp>
2025-02-17 16:42:29 +0900

    1st commit

--- sample.py
+++ sample.py
@@ -1,14 +1,10 @@
-#必要なモジュールのインポート
 from flask import Flask
 
-#　Flask をインスタンス化
 app = Flask(__name__)
 
-# ルートディレクトリにアクセスがあった時の処理
-@app.route('/') 
+@app.route('/')
 def hello():
     return 'Hello World!'
 
-# エントリーポイント
 if __name__ == '__main__':
     app.run()
